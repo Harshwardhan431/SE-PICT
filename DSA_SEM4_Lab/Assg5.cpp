37a58f528bfc492c907e40905c281fcdd70ea9e0
Harshwardhan Atkare <81025973+Harshwardhan431@users.noreply.github.com>
2022-04-23 00:19:01 +0530

    Update Assg5.cpp

--- DSA_SEM4_Lab/Assg5.cpp
+++ DSA_SEM4_Lab/Assg5.cpp
@@ -1,126 +1,156 @@
-#include<iostream>
+#include <iostream>
+#include<string>
+#include<string.h>
+#include<algorithm>
 using namespace std;
 
-struct Node{
-	int data;
-	Node* next;
+const int SIZE = 29;
+
+class HashEntry{
+    string word,meaning;
+    HashEntry *next;
 public:
-	friend class LinkedList;
-};
-
-class LinkedList{
-private:
-    Node *first;
- public:
-    friend class ChainHash;
-    LinkedList(){first=nullptr;}
-    ~LinkedList();
-
-    void Display();
-    void Insert(int index,int x);
-    int Delete(int index);
-    int Length();
-};
-
-void LinkedList::Display()
-{
-    Node *p=first;
-
- while(p)
- {
- cout<<p->data<<" ";
- p=p->next;
- }
- cout<<endl;
-}
-
-int LinkedList::Length()
-{
-    Node *p=first;
- int len=0;
-
- while(p)
- {
- len++;
- p=p->next;
- }
- return len;
-}
-
-void LinkedList::Insert(int index,int x)
-{
-	Node *t,*p=first;
-
-	   if(index <0 || index > Length())
-	     return;
-	   t=new Node;
-	   t->data=x;
-	   t->next=nullptr;
-
-	   if(index==0)
-	   {
-	     t->next=first;
-	     first=t;
-	   }
-	   else
-	   {
-	     for(int i=0;i<index-1;i++)
-	         p=p->next;
-	     t->next=p->next;
-	     p->next=t;
-	   }
-}
-
-class ChainHash{
-private:
-	int bucket;
-	LinkedList *l;
-public:
-	ChainHash(int b){
-		bucket=b;
-
-	}
-
-	int hashFunction(int x) {
-        return (x % bucket);
+    HashEntry(){
+        word = meaning = "";
+        next = nullptr;
     }
 
-	void insertItem(int key)
-	{
-		cout<<"euu"<<endl;
-		int index = hashFunction(key);
-		int length=l->Length();
-		cout<<index<<" "<<length;
-		l[index].Insert((length-1), key);
-	}
-
-	void printHash(){
-		for (int i = 0; i < bucket; i++) {
-		    cout << i;
-		    l[i].Display();
-		    cout<<endl;
-	}
-	}
+    HashEntry(string w,string m){
+        word = w;
+        meaning = m;
+        next = nullptr;
+    }
+    friend class Dictionary;
 };
 
-int main(){
-	ChainHash c(10);
-	cout<<"Enter 1 to insert value"<<endl;
-	cout<<"Enter 2 to print linkedlist"<<endl;
-	cout<<"Enter -1 to exit"<<endl;
-	int choice;
-	cin>>choice;
-	while(1){
-		if (choice==1){
-			int x;
-			cout<<"Enter value to be inserted"<<endl;
-			cin>>x;
-			c.insertItem(x);
-		}else if (choice==2){
-			c.printHash();
-		}else if (choice==-1){
-			break;
-		}
-		}
-	return 0;
-	}
+class Dictionary{
+    HashEntry* ht[SIZE];
+public:
+    Dictionary(){
+        for(int i = 0;i < SIZE;i++)
+            ht[i] = nullptr;
+    }
+
+    int HashFunc(string word){
+        int res = 0;
+        for(int i = 0;i < (int)word.length();i++){
+            res += (word[i] - 'a')*i;
+        }
+        return res%SIZE;
+    }
+
+    void insert(string w,string m){
+        HashEntry* new1 = new HashEntry(w,m);
+        int hi = HashFunc(w);
+
+        if(ht[hi] == nullptr){  
+            ht[hi] = new1;
+            return;
+        }
+        HashEntry* curr = ht[hi];
+        while(curr->next != nullptr){
+            curr = curr->next;
+        }
+        curr->next = new1;
+    }
+
+    void display(){
+        for(int i = 0;i < SIZE;i++){
+            if(ht[i]){
+                HashEntry *curr = ht[i];
+                cout << i << ": ";
+                while(curr){
+                    cout << "(" << curr->word << "," << curr->meaning << ") ->";
+                    curr = curr->next;
+                }
+                cout << "\n";
+            }
+        }
+    }
+
+    void search(string key,int &cnt){
+        int hi = HashFunc(key);
+        cnt = -1;
+        if(ht[hi] == nullptr){
+            return;
+        }
+
+        HashEntry *curr = ht[hi];
+        cnt = 0;
+        while(curr != nullptr){
+            cnt++;
+            if(curr->word == key){
+                return;
+            }
+            curr = curr->next;
+        }
+        cnt = -1;
+        return;
+    }
+
+    void find(string w){
+        int c;
+        search(w,c);
+        if(c == -1){
+            cout << "Element " << w <<" not found in the dictionary" << "\n";
+            return;
+        }
+        cout << "Element " << w << " found in the dictionary :- " << "\n";
+        cout << "Word :- " << w << " Comparisions :- " << c << "\n";
+        return;
+    }
+
+    void remove(string w){
+        int c;
+        search(w,c);
+        
+        if(c == -1){
+            cout << "Element " << w <<" not found in the dictionary" << "\n";
+            return;
+        }
+
+        int hi = HashFunc(w);
+        HashEntry *curr = ht[hi],*prev = nullptr;
+        
+        if(c == 1){
+            ht[hi] = curr->next;
+            delete curr;
+            cout << "Deletion successfull" << "\n";
+            return;
+        }
+
+        while(curr->word != w){
+            prev = curr;
+            curr = curr->next;
+        }
+
+        prev->next = curr->next;
+        delete curr;
+
+        cout << "Deletion successfull" << "\n";
+
+        return;
+    }
+
+};
+
+int main() {
+
+    Dictionary obj;
+    obj.insert("abc","1");
+    obj.insert("bcd","2");
+    obj.insert("cde","3");
+    obj.insert("def","4");
+    obj.insert("efg","5");
+    obj.insert("abc","6");
+    obj.insert("def","7");
+    obj.find("abc");
+    obj.find("efg");
+    obj.display();
+    obj.remove("abc");
+    obj.remove("abc");
+    obj.remove("xyz");
+    obj.display();
+
+    return 0;
+}
